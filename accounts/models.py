30c7a98e9e4ad2eeedaa191cf6ba0920eacee6b9
picklehari <harirajmanghat123@gmail.com>
2018-07-28 20:09:25 +0530

    Developer

--- accounts/models.py
+++ accounts/models.py
@@ -2,29 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from testApps.models import testapps
-import datetime
+from testApps.models import testApp
+import django.utils.timezone
+
 class userProfile(models.Model):
-    ratingsChoices =(
-        ('1' , 'veryBad'),
-        ('2', 'bad'),
-        ('3', 'okay'),
-        ('4', 'good'),
-        ('5', 'excellent')
-    )
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     userTags = models.CharField(max_length=1500,null=True,blank=True)
-    applicationID = models.ForeignKey(testapps, on_delete=models.CASCADE)
-    deviceDetails = models.CharField()   
-    appUsedFirstOn = models.DateField(default = datetime.datetime.today())
+    applicationID = models.ForeignKey(testApp, on_delete=models.CASCADE)
+    deviceDetails = models.TextField(max_length=50000)   
+    appUsedFirstOn = models.DateField(default = django.utils.timezone.now)
     applicationFinalReview = models.TextField(max_length = 2500, blank = True, null=True,default='')    
     @receiver(post_save,sender=User)
-    def create_user_profile(sender,instance,created,**kwargs):
+    def create_user_profile(self,sender,instance,created,**kwargs):
         if created:
-            testUser.objects,create(user = instance)
+            self.objects.create(user = instance)
         
     @receiver(post_save,sender=User)
-    def save_user_profile(sender,instance,**kwargs):
+    def save_user_profile(self,sender,instance,**kwargs):
         instance.profile.save()
 
     def getApplicatonID(self):
@@ -36,4 +30,3 @@ class userProfile(models.Model):
     def getReview(self,appReview):
         self.applicationReview = appReview
 
-    
--- developer/admin.py
+++ developer/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import testApp
+from .models import developer
 # Register your models here.
-admin.site.register(testApp)
+admin.site.register(developer)
